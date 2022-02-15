dbb0788979cbcf7c48126cb2213805afa205bb35
Breyner Rojas <mon2bdr@gmail.com>
2022-02-15 11:56:39 -0500

    Create Gradle project and send a message to a topic

---
+++ KafkaCourse/src/main/java/com/github/tavet/kafka/Producer.java
@@ -0,0 +1,28 @@
+package com.github.tavet.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class Producer {
+    public static void main(String[] args) {
+
+        // List of properties https://kafka.apache.org/documentation/#producerconfigs
+        // Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // async
+        producer.send(new ProducerRecord<>("first_topic", "test from Java"));
+
+        producer.flush();
+        producer.close();
+    }
+}
